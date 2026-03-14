6ba6d68db89cd8daff4a3c7f0af33c3ffb24341f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2026-03-14 20:05:56 -0300

    Initialize pump-data-service project with Maven and Spring Boot setup

---
+++ pump-data-service/src/main/java/com/company/pump_data_service/PumpDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.pump_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpDataServiceApplication.class, args);
+	}
+
+}
---
+++ pump-data-service/src/test/java/com/company/pump_data_service/PumpDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.pump_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
